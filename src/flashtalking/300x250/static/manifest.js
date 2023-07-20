fe5f8054f2aa3caad996e53055d6c2eaf2c01b62
Luis Carlos Palomo <42524903+PalomoLuis@users.noreply.github.com>
2023-07-20 14:25:33 -0300

    customElement exandable flashtalking template

---
+++ src/flashtalking/300x250/static/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+  "filename": "index.html",
+  "width": 300,
+  "height": 250,
+  "clickTagCount": 1,
+  "hideBrowsers": ["ie8"],
+  "expand":{
+    "fullscreen":false,
+    "width":500,
+    "height":300,
+    "indentAcross":0,
+    "indentDown":0
+  }
+});
